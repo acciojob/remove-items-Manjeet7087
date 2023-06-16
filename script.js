df8e641c5a98a20bb10b5a2951602bdfb5014e84
Manjeet7087 <131547570+Manjeet7087@users.noreply.github.com>
2023-06-16 21:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeColor() {
+  var colorSelect = document.getElementById('colorSelect');
+  var selectedValue = colorSelect.value;
+  
+  if (selectedValue !== '') {
+    var selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    colorSelect.removeChild(selectedOption);
+  }
+}
+
